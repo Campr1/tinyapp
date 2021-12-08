241d8bcbbcf96c369b4d6808f8872f663c103dda
Rishard C <rishard.c@hotmail.com>
2021-12-07 21:10:47 -0500

    add cookies and user name

--- express_server.js
+++ express_server.js
@@ -1,87 +1,97 @@
-const { request } = require("express");
-const express = require("express");
+const express = require ('express');
 const app = express();
-const PORT = 8080; // default port 8080
+const PORT = 8080;
+
 const bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({extended: true}));
 
-const cookieParser = require('cookie-parser')
-app.use(cookieParser())
-
-
-
-// set ejs as the template engine
-app.set("view engine", "ejs");
-
-// generate random alpha-numeric string for shortURL
-function generateRandomString() {
-  return Math.random().toString(20).substr(2, 6)
-};
+const cookieParser = require('cookie-parser');
+app.use(cookieParser());
 
 const urlDatabase = {
-  "b2xVn2": "http://www.lighthouselabs.ca",
+  b2xVn2: "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
 
-//Routes
+function generateRandomString() {
+    return Math.random().toString(20).substr(2, 6)
+};
+
+app.set('view engine', 'ejs')
+
 app.get('/', function (req, res) {
   // Cookies that have not been signed
   console.log('Cookies: ', req.cookies)
+ 
 });
 
-app.get("/", (req, res) => {
-  res.send("Hello!");
+app.get("/", (req, res,) => {
+  res.send("Welcome to Tinyapp");
 });
 
-app.get("/urls.json", (req, res) => {
+app.get('/urls.json', (req, res) => {
   res.json(urlDatabase);
 });
 
-app.get("/urls", (req, res) => {
-  const templateVars = { urls: urlDatabase };
-  res.render("urls_index", templateVars);
+app.get('/hello', (req, res) => {
+  res.send('<html><body>Hello <b>World</b></body></html>\n')
 });
 
-app.get("/hello", (req, res) => {
-  res.send("<html><body>Hello <b>World</b></body></html>\n");
+app.get('/urls', (req,res) => {
+  const templateVars = { 
+    urls: urlDatabase, 
+    username: req.cookies["username"]
+};
+  res.render('urls_index', templateVars);
 });
 
 app.get("/urls/new", (req, res) => {
-  res.render("urls_new");
+  const templateVars = { 
+    username: req.cookies["username"]
+}
+  res.render("urls_new", templateVars);
 });
 
 app.get("/urls/:shortURL", (req, res) => {
-  const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
+  const shortURL = req.params.shortURL;
+  //What would happen if a client requests a non-existent shortURL?
+  if (!urlDatabase[shortURL]) {
+    res.send('<html><body>Error</body></html>\n');
+    return;
+  }
+  const templateVars = { 
+    shortURL: shortURL, 
+    longURL: urlDatabase[req.params.shortURL],
+    username: req.cookies["username"]
+  };
   res.render("urls_show", templateVars);
 });
 
-app.post("/urls", (req, res) => {
-  console.log(req.body);  
-  let randomStr = generateRandomString();
-
-  // save shortURL & longURL to db when post request is recieved
-  urlDatabase[randomStr] = req.params.longURL;
-  res.render(`/urls/${randomStr}`); //redirect
-
-});
- //redirect shortURL to longURL
-app.get("/u/:shortURL", (req, res) => {
-  const longURL = urlDatabase[req.params.shortURL]
+app.get('/u/:shortURL', (req, res) => {
+  const longURL = urlDatabase[req.params.shortURL];
   res.redirect(longURL);
 });
 
+app.post("/urls", (req, res) => {
+  console.log(req.body);  // Log the POST request body to the console
+  let newID = generateRandomString();
+  urlDatabase[newID] = req.body.longURL;
+
+  const templateVars = { 
+    username: req.cookies["username"]
+}
+  res.redirect(`/urls/${newID}`, templateVars);         // Respond redirect
+});
+
 app.post("/urls/:shortURL/delete", (req, res) => {
-  delete urlDatabase[req.params.shortURL]
-  res.redirect("/urls");
+  const shortURL = req.params.shortURL;
+  delete urlDatabase[shortURL];
+  res.redirect(`/urls`);         // Respond redirect to index page
 });
 
 app.post("/urls/:id", (req, res) => {
   const id = req.params.id;
   urlDatabase[id] = req.body.longURL;
-  res.redirect(`/urls`);         
-});
 
-app.listen(PORT, () => {
-  console.log(`Example app listening on port ${PORT}!`);
+  res.redirect
 });
-
